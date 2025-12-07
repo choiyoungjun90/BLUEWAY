a2390a1da38b142b3b704331bb722113df542e13
choiyoungjun90 <xxgf123@gmail.com>
2025-12-07 22:06:27 +0900

    Add netlify.toml for build config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/', // 기본값이므로 생략 가능하지만, 혹시 다른 값으로 되어 있다면 확인 필요
+  base: '/', 
+  build: {
+    outDir: 'dist',
+  }
 })
