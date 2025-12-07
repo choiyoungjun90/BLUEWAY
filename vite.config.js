de1b1be202bafade4e1cc8791e483d7d79b3fa63
choiyoungjun90 <xxgf123@gmail.com>
2025-12-07 22:00:02 +0900

    feat: _redirects

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: '/', // 기본값이므로 생략 가능하지만, 혹시 다른 값으로 되어 있다면 확인 필요
 })
