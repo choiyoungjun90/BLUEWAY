ebee4f357731ddc5f52edfdeee3a76a9e271c2f4
choiyoungjun <choiyoungjunMindsai>
2025-12-08 09:45:58 +0900

    Fix vite

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // '/' 대신 './' 로 변경
+  base: '/',  // Netlify 루트 배포에 맞게 변경
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  }
+  },
+  css: {
+    postcss: './postcss.config.js',
+  },
 })
